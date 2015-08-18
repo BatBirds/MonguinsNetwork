dbca291bdbb35613e431266856aae5f00fe2edfb
Tornady <clbw@itu.dk>
2015-08-18 11:33:22 +0200

    FPS Monguins

---
+++ Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour {
+
+    [SyncVar]
+    Quaternion syncPlayerRotation;
+    [SyncVar]
+    Quaternion syncCamRotation;
+
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Transform camTransform;
+    [SerializeField]
+    float lerpRate = 15;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        TransmitRotations();
+        LerpRotations();
+	}
+
+    void LerpRotations() {
+        if (!isLocalPlayer) {
+            playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation, Time.deltaTime * lerpRate);
+            camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvideRotationsToServer(Quaternion playerRot, Quaternion camRot) {
+        syncPlayerRotation = playerRot;
+        syncCamRotation = camRot;
+    }
+
+    [Client]
+    void TransmitRotations() {
+        if (isLocalPlayer) {
+            CmdProvideRotationsToServer(playerTransform.rotation, camTransform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/_PlayerSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class _PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Camera FPSCharacterCam;
+
+    // Use this for initialization
+    void Start() {
+        if (isLocalPlayer) {
+            GameObject.Find("Scene Camera").SetActive(false);
+            GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = true;
+            FPSCharacterCam.enabled = true;
+        }
+    }
+}
